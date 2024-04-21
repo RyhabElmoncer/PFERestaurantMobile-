14a5cee853827009761d1c15d529a7c5596d4da1
ryhab <ryhabelmoncerr@gmail.com>
2024-04-21 22:28:07 +0100

    first commit

---
+++ android/app/src/main/java/com/example/gestionrestaurant/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.gestionrestaurant;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gestionrestaurant/presentation/resources/strings_manager.dart';
+
+import '../splash/splash_view.dart';
+
+class Routes {
+  static const String splashRoute = "/";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgotPasswordRoute = "/forgotPassword";
+  static const String onBoardingRoute = "/onBoarding";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+    /*  case Routes.loginRoute:
+        initLoginModule();
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.registerRoute:
+        initRegisterModule();
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        initForgotPasswordModule();
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.mainRoute:
+        initHomeModule();
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        initStoreDetailsModule();
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());*/
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: Text(AppStrings.noRouteFound.trim()),
+              ),
+              body: Center(child: Text(AppStrings.noRouteFound.trim())),
+            ));
+  }
+}
